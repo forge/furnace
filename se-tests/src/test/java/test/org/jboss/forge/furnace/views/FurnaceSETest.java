1794a70b6ccf84b71403e1f36bd87bd652100f90
Lincoln Baxter, III <lincolnbaxter@gmail.com>
2013-07-15 21:05:45 -0400

    Upgrade to new AddonManager API

--- se-tests/src/test/java/test/org/jboss/forge/furnace/views/FurnaceSETest.java
+++ se-tests/src/test/java/test/org/jboss/forge/furnace/views/FurnaceSETest.java
@@ -13,9 +13,8 @@ import java.util.concurrent.TimeUnit;
 import org.jboss.forge.addon.manager.AddonManager;
 import org.jboss.forge.addon.manager.impl.AddonManagerImpl;
 import org.jboss.forge.addon.manager.impl.request.ConfigurationScanListener;
-import org.jboss.forge.addon.maven.dependencies.FileResourceFactory;
-import org.jboss.forge.addon.maven.dependencies.MavenContainer;
-import org.jboss.forge.addon.maven.dependencies.MavenDependencyResolver;
+import org.jboss.forge.addon.manager.spi.AddonDependencyResolver;
+import org.jboss.forge.addon.maven.addon.MavenAddonDependencyResolver;
 import org.jboss.forge.furnace.Furnace;
 import org.jboss.forge.furnace.addons.Addon;
 import org.jboss.forge.furnace.addons.AddonId;
@@ -49,9 +48,8 @@ public class FurnaceSETest
 
       furnace.addRepository(AddonRepositoryMode.MUTABLE, repodir1);
 
-      MavenDependencyResolver resolver = new MavenDependencyResolver(new FileResourceFactory(),
-               new MavenContainer());
-      AddonManager manager = new AddonManagerImpl(furnace, resolver);
+      AddonDependencyResolver resolver = new MavenAddonDependencyResolver();
+      AddonManager manager = new AddonManagerImpl(furnace, resolver, false);
 
       AddonId projects = AddonId.from("org.jboss.forge.addon:projects", "2.0.0-SNAPSHOT");
       AddonId maven = AddonId.from("org.jboss.forge.addon:maven", "2.0.0-SNAPSHOT");
