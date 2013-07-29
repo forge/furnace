14ec29956d61fd9b97ba501bb059c3fc4ac49ce9
Lincoln Baxter, III <lincolnbaxter@gmail.com>
2013-07-29 13:47:34 -0400

    Provide AddonRepository JavaDoc

--- container-api/src/main/java/org/jboss/forge/furnace/repositories/AddonRepository.java
+++ container-api/src/main/java/org/jboss/forge/furnace/repositories/AddonRepository.java
@@ -24,25 +24,61 @@ import org.jboss.forge.furnace.versions.Version;
  */
 public interface AddonRepository
 {
+   /**
+    * Get the base installation directory of the given {@link AddonId}.
+    */
    public File getAddonBaseDir(AddonId addon);
 
+   /**
+    * Get the runtime {@link Addon} dependencies of the given {@link AddonId}.
+    */
    public Set<AddonDependencyEntry> getAddonDependencies(AddonId addon);
 
+   /**
+    * Get the addon descriptor file for the given {@link AddonId}.
+    */
    public File getAddonDescriptor(AddonId addon);
 
+   /**
+    * Get a list of all local resources for the given {@link AddonId}.
+    */
    public List<File> getAddonResources(AddonId addon);
 
+   /**
+    * Get the root directory of this {@link AddonRepository}.
+    */
    public File getRootDirectory();
 
+   /**
+    * Returns <code>true</code> if the given {@link AddonId} is deployed in this {@link AddonRepository}; otherwise,
+    * returns <code>false</code>.
+    */
    public boolean isDeployed(AddonId addon);
 
+   /**
+    * Returns <code>true</code> if the given {@link AddonId} is enabled in this {@link AddonRepository}; otherwise,
+    * returns <code>false</code>.
+    */
    public boolean isEnabled(final AddonId addon);
 
+   /**
+    * Returns a {@link List} of {@link AddonId} instances for all enabled {@link Addon}s in this repository.
+    */
    public List<AddonId> listEnabled();
 
+   /**
+    * Returns a {@link List} of {@link AddonId} instances for all enabled {@link Addon}s in this repository that are API
+    * compatible with the given {@link Version}.
+    */
    public List<AddonId> listEnabledCompatibleWithVersion(final Version version);
 
+   /**
+    * Returns the last modified date of this {@link AddonRepository}.
+    */
    public Date getLastModified();
 
+   /**
+    * Returns the runtime change version of this {@link AddonRepository}.
+    */
    public int getVersion();
 }
