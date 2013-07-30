1be0057c076ee68215436a697fb035eb8344677e
Lincoln Baxter, III <lincolnbaxter@gmail.com>
2013-07-30 16:35:02 -0400

    Better (more thorough) addon shutdown algorithm

--- container/src/main/java/org/jboss/forge/furnace/impl/graph/MasterGraph.java
+++ container/src/main/java/org/jboss/forge/furnace/impl/graph/MasterGraph.java
@@ -201,7 +201,6 @@ public class MasterGraph
       return builder.toString();
    }
 
-   @SuppressWarnings("resource")
    public void toDOT(File file)
    {
       FileWriter fw = null;
--- container/src/main/java/org/jboss/forge/furnace/impl/graph/MasterGraphChangeHandler.java
+++ container/src/main/java/org/jboss/forge/furnace/impl/graph/MasterGraphChangeHandler.java
@@ -8,6 +8,7 @@ import org.jboss.forge.furnace.addons.AddonView;
 import org.jboss.forge.furnace.impl.addons.AddonLifecycleManager;
 import org.jgrapht.event.TraversalListenerAdapter;
 import org.jgrapht.event.VertexTraversalEvent;
+import org.jgrapht.traverse.BreadthFirstIterator;
 import org.jgrapht.traverse.DepthFirstIterator;
 
 public class MasterGraphChangeHandler
@@ -28,6 +29,7 @@ public class MasterGraphChangeHandler
    {
       initGraph();
       markDirty();
+      markRemovedDirty();
       stopDirty();
       stopRemoved();
       loadAddons();
@@ -184,26 +186,7 @@ public class MasterGraphChangeHandler
          iterator.next();
    }
 
-   private void stopDirty()
-   {
-      DepthFirstIterator<AddonVertex, AddonDependencyEdge> iterator = new DepthFirstIterator<AddonVertex, AddonDependencyEdge>(
-               graph.getGraph());
-
-      iterator.addTraversalListener(new TraversalListenerAdapter<AddonVertex, AddonDependencyEdge>()
-      {
-         @Override
-         public void vertexTraversed(VertexTraversalEvent<AddonVertex> event)
-         {
-            if (event.getVertex().isDirty())
-               lifecycleManager.stopAddon(event.getVertex().getAddon());
-         };
-      });
-
-      while (iterator.hasNext())
-         iterator.next();
-   }
-
-   private void stopRemoved()
+   private void markRemovedDirty()
    {
       if (lastMasterGraph != null)
       {
@@ -231,8 +214,59 @@ public class MasterGraphChangeHandler
 
                if (!exists)
                {
-                  lifecycleManager.stopAddon(lastVertex.getAddon());
+                  lastVertex.setDirty(true);
+                  Set<AddonDependencyEdge> edges = lastMasterGraph.getGraph().incomingEdgesOf(lastVertex);
+                  for (AddonDependencyEdge edge : edges)
+                  {
+                     AddonVertex source = lastMasterGraph.getGraph().getEdgeTarget(edge);
+                     Set<AddonVertex> vertices = graph.getVertices(source.getName(), source.getVersion());
+                     for (AddonVertex vertex : vertices)
+                     {
+                        vertex.setDirty(true);
+                     }
+                  }
                }
+            };
+         });
+
+         while (iterator.hasNext())
+            iterator.next();
+      }
+   }
+
+   private void stopDirty()
+   {
+      BreadthFirstIterator<AddonVertex, AddonDependencyEdge> iterator = new BreadthFirstIterator<AddonVertex, AddonDependencyEdge>(
+               graph.getGraph());
+
+      iterator.addTraversalListener(new TraversalListenerAdapter<AddonVertex, AddonDependencyEdge>()
+      {
+         @Override
+         public void vertexTraversed(VertexTraversalEvent<AddonVertex> event)
+         {
+            if (event.getVertex().isDirty())
+               lifecycleManager.stopAddon(event.getVertex().getAddon());
+         };
+      });
+
+      while (iterator.hasNext())
+         iterator.next();
+   }
+
+   private void stopRemoved()
+   {
+      if (lastMasterGraph != null)
+      {
+         BreadthFirstIterator<AddonVertex, AddonDependencyEdge> iterator = new BreadthFirstIterator<AddonVertex, AddonDependencyEdge>(
+                  lastMasterGraph.getGraph());
+
+         iterator.addTraversalListener(new TraversalListenerAdapter<AddonVertex, AddonDependencyEdge>()
+         {
+            @Override
+            public void vertexTraversed(VertexTraversalEvent<AddonVertex> event)
+            {
+               if (event.getVertex().isDirty())
+                  lifecycleManager.stopAddon(event.getVertex().getAddon());
             };
          });
 
