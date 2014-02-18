a123affd5dd7ecf8b79034497e3f19b7789ad152
Lincoln Baxter, III <lincolnbaxter@gmail.com>
2014-02-18 11:56:53 -0500

    Reproduced compatible classloader issue (potentially solvable using whitelist)

---
+++ proxy-tests/src/test/java/org/jboss/forge/classloader/whitelist/MockContext.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright 2014 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Eclipse Public License version 1.0, available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.jboss.forge.classloader.whitelist;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author <a href="mailto:lincolnbaxter@gmail.com">Lincoln Baxter, III</a>
+ * 
+ */
+public class MockContext
+{
+   private final Map<Object, Object> attributes = new HashMap<>();
+
+   public Map<Object, Object> getAttributes()
+   {
+      return attributes;
+   }
+}
---
+++ proxy-tests/src/test/java/org/jboss/forge/classloader/whitelist/MockContextConsumer.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2014 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Eclipse Public License version 1.0, available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.jboss.forge.classloader.whitelist;
+
+import org.jboss.forge.furnace.proxy.Proxies;
+
+/**
+ * @author <a href="mailto:lincolnbaxter@gmail.com">Lincoln Baxter, III</a>
+ * 
+ */
+public class MockContextConsumer
+{
+   public void processContext(MockContext context)
+   {
+      MockContextPayload payload = (MockContextPayload) context.getAttributes().get(
+               MockContextPayload.class.getName());
+
+      // this payload should not be proxied with the CLAC, since its interface is available in this classloader.
+      if (Proxies.isForgeProxy(payload))
+         throw new IllegalStateException("Should not have been a proxy");
+   }
+
+}
