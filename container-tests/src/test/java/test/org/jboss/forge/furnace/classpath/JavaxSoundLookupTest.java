ec13eb4507417502ce15c0da75f7331c0065a75b
George Gastaldi <gegastaldi@gmail.com>
2014-03-13 14:02:00 -0300

    Pushing test reported by aslak

---
+++ container-tests/src/test/java/test/org/jboss/forge/furnace/classpath/JavaxSoundLookupTest.java
@@ -0,0 +1,59 @@
+package test.org.jboss.forge.furnace.classpath;
+
+import java.util.ServiceLoader;
+
+import javax.sound.midi.spi.MidiDeviceProvider;
+import javax.sound.sampled.spi.MixerProvider;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.forge.arquillian.archive.ForgeArchive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(Arquillian.class)
+public class JavaxSoundLookupTest
+{
+   @Deployment
+   public static ForgeArchive getDeployment()
+   {
+      ForgeArchive archive = ShrinkWrap.create(ForgeArchive.class)
+               .addAsLocalServices(JavaxSoundLookupTest.class);
+
+      return archive;
+   }
+
+   @Test
+   public void testGetJDKProvidedImpl() throws Exception
+   {
+      try
+      {
+         getClass().getClassLoader().loadClass("com.sun.media.sound.RealTimeSequencerProvider");
+         getClass().getClassLoader().loadClass("com.sun.media.sound.MidiOutDeviceProvider");
+         getClass().getClassLoader().loadClass("com.sun.media.sound.MidiInDeviceProvider");
+         getClass().getClassLoader().loadClass("com.sun.media.sound.SoftProvider");
+      }
+      catch (Exception e)
+      {
+         Assert.fail("Could not load required Factory class." + e.getMessage());
+      }
+   }
+
+   @Test
+   @Ignore("This test fails")
+   public void testMidiDeviceProviderLookup()
+   {
+      Assert.assertTrue(ServiceLoader.load(MidiDeviceProvider.class).iterator().hasNext());
+   }
+
+   @Test
+   @Ignore("This test fails")
+   public void testMixerProviderLookup()
+   {
+      Assert.assertTrue(ServiceLoader.load(MixerProvider.class).iterator().hasNext());
+   }
+
+}
