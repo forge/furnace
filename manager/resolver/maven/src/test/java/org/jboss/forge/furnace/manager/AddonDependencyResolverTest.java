36ce615b44ce8adef02607951ce0f16536dc8cdc
George Gastaldi <gegastaldi@gmail.com>
2014-07-28 16:56:48 -0300

    Renamed test class

--- manager/resolver/maven/src/test/java/org/jboss/forge/furnace/manager/AddonDependencyResolverTest.java
+++ manager/resolver/maven/src/test/java/org/jboss/forge/furnace/manager/AddonDependencyResolverTest.java
@@ -26,7 +26,7 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-public class AddonManagerDependencyResolutionTest
+public class AddonDependencyResolverTest
 {
 
    @BeforeClass
