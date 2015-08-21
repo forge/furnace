a8a1cfd7819048573db0e14401646d88434edb60
George Gastaldi <gegastaldi@gmail.com>
2015-08-21 14:00:43 -0300

    Improved javadoc

--- container-api/src/main/java/org/jboss/forge/furnace/spi/ExportedInstance.java
+++ container-api/src/main/java/org/jboss/forge/furnace/spi/ExportedInstance.java
@@ -9,15 +9,37 @@ package org.jboss.forge.furnace.spi;
 import org.jboss.forge.furnace.addons.Addon;
 
 /**
+ * An {@link ExportedInstance} object is a service returned from {@link ServiceRegistry}
+ * 
  * @author <a href="mailto:lincolnbaxter@gmail.com">Lincoln Baxter, III</a>
+ * @author <a href="mailto:ggastald@redhat.com">George Gastaldi</a>
  */
 public interface ExportedInstance<T>
 {
+   /**
+    * @return the instance associated with this {@link ExportedInstance}
+    */
    T get();
 
+   /**
+    * Releases this {@link ExportedInstance}. Subsequent calls to {@link ExportedInstance#get()} may present a different
+    * behavior
+    * 
+    * @param instance the instance returned in this{@link ExportedInstance#get()}
+    */
    void release(T instance);
 
+   /**
+    * The type associated with this {@link ExportedInstance}
+    * 
+    * @return {@link Class} associated with this object
+    */
    Class<? extends T> getActualType();
 
+   /**
+    * The addon that created this {@link ExportedInstance}
+    * 
+    * @return the {@link Addon} instance that created this object
+    */
    Addon getSourceAddon();
 }
