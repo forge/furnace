3ebd113cf0a750429e22ae37fb81aa29df09afe0
George Gastaldi <gegastaldi@gmail.com>
2016-02-01 13:32:43 -0200

    Improved javadoc

--- container/src/main/java/org/jboss/forge/furnace/impl/modules/ModuleSpecProvider.java
+++ container/src/main/java/org/jboss/forge/furnace/impl/modules/ModuleSpecProvider.java
@@ -10,7 +10,21 @@ import org.jboss.modules.ModuleIdentifier;
 import org.jboss.modules.ModuleLoader;
 import org.jboss.modules.ModuleSpec;
 
+/**
+ * A {@link ModuleSpecProvider} is used by {@link AddonModuleLoader} to provide {@link ModuleSpec} instances based on a
+ * given {@link ModuleIdentifier}
+ *
+ * @author <a href="mailto:lincolnbaxter@gmail.com">Lincoln Baxter, III</a>
+ */
 public interface ModuleSpecProvider
 {
+   /**
+    * Returns a {@link ModuleSpec} instance given a {@link ModuleIdentifier}
+    * 
+    * @param loader the {@link ModuleLoader} used to load the returned {@link ModuleSpec}
+    * @param id the {@link ModuleIdentifier} of the loaded {@link ModuleSpec}
+    * @return the {@link ModuleSpec} associated with this {@link ModuleIdentifier}, <code>null</code> if the
+    *         {@link ModuleIdentifier} does not apply to this {@link ModuleSpecProvider}
+    */
    ModuleSpec get(ModuleLoader loader, ModuleIdentifier id);
 }
